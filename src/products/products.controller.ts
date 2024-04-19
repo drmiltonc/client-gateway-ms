adad055672069921cd4e1d74291c73a1292c2a65
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-04-18 19:40:49 -0500

    obtener todos los productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Patch, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PRODUCT_SERVICE } from 'src/config/services';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+
+  constructor(@Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy) {
+
+  }
 
   @Post()
   createProduct() {
@@ -11,7 +16,7 @@ export class ProductsController {
 
   @Get()
   findAllProducts() {
-    return 'Esta función regresa varios productos';
+    return this.productsClient.send({ cmd: 'find_all_products' }, {});
   }
 
   @Get(':id')
